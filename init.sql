1e3360169c644e266392a27d12003c9d865d8570
henda <farhanii.henda@gmail.com>
2025-07-09 18:04:36 +0200

    Optimize the sql-init.sql file

--- init.sql
+++ init.sql
@@ -1,4 +1,11 @@
 -- init.sql
-ALTER USER 'root'@'%' IDENTIFIED BY 'PASSWORD';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+ALTER USER 'root'@'%' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON . TO 'root'@'%' WITH GRANT OPTION;
+
+CREATE DATABASE IF NOT EXISTS ride_sharing CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE USER 'microgo_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON ride_sharing.* TO 'microgo_user'@'%';
+
 FLUSH PRIVILEGES;
